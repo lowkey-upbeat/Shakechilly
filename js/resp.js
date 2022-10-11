2518748d85ea5efe6bebbcb269e3750fbf52e962
Shreyansh-27 <shreyanshbhardwaj.cse20@ggct.co.in>
2022-10-11 15:17:43 +0530

    added aboutus

--- js/resp.js
+++ js/resp.js
@@ -9,3 +9,18 @@ burger.addEventListener('click', ()=>{
   cart.classList.toggle('v-class-resp');
   navbar.classList.toggle('h-nav-resp');
 })
+
+
+function mufunction(){
+  window.scrollTo(0,
+          findPosition(document.getElementById("services")));
+            }
+            function findPosition(obj) {
+                var currenttop = 0;
+                if (obj.offsetParent) {
+                    do {
+                        currenttop += obj.offsetTop;
+                    } while ((obj = obj.offsetParent));
+                    return [currenttop];
+                }
+}
